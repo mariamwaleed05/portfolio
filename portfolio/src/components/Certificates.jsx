a3bc697495fae5003ab60f0629c192880a28eee1
mariamwaleed05 <mariam.waleed2005@gmail.com>
2025-11-14 22:27:57 +0200

    Certification Section / About

--- portfolio/src/components/Certificates.jsx
+++ portfolio/src/components/Certificates.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Group from '../imgs/Group 14002.png';
 
 const Certificates = () => {
   return (
     <>
       <h2 className="name-basket bbt">Certificates & Recognition</h2>
-      
+  <img src={Group} alt="id" className="awwards" />
     </>
   );
 };
