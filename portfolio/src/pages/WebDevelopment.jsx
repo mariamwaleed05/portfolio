7b6192a2570b59221570ab357f93c3ecace0d367
mariamwaleed05 <mariam.waleed2005@gmail.com>
2025-12-16 18:35:55 +0200

    fix: coding service page / connected to api

--- portfolio/src/pages/WebDevelopment.jsx
+++ portfolio/src/pages/WebDevelopment.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from "react-helmet";
-
 import SideMenu from '../common/SideMenu';
 import ToggleButtons from './../components/ToggleButtons';
 import BackToTop from '../components/BackToTop';
@@ -8,27 +7,48 @@ import TextMeButton from '../components/TextMeButton';
 import Footer from '../common/Footer';
 import PreLoader from './../common/PreLoader';
 import ServiceProjects from './../components/ServiceProjects';
-import { webDevelopmentProjects } from './../data/services/webDevelopmentProjects';
+import { fetchProjectsByCategory } from '../data/services/api';
 
 const WebDevelopment = () => {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadProjects = async () => {
+            try {
+                const data = await fetchProjectsByCategory('WebDevelopment');
+                setProjects(data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadProjects();
+    }, []);
+
+    if (loading) return <PreLoader />;
 
     return ( 
         <>
-        <Helmet><title>Web Development</title>
-             <link rel="icon" type="image/png" href="/icon.png" sizes="16x16" />    
-        </Helmet>
-        <PreLoader/>
-        <SideMenu/>
-        <ToggleButtons/>
-
-<div className='main-wrapper'>
-        <ServiceProjects serviceName="Web Development" serviceColor="#690600" projects={webDevelopmentProjects} />;
-
-</div>
-
-        <Footer/>
-        <BackToTop/>
-        <TextMeButton/>
+            <Helmet>
+                <title>Web Development</title>
+                <link rel="icon" type="image/png" href="/icon.png" sizes="16x16" />    
+            </Helmet>
+            <SideMenu/>
+            <ToggleButtons/>
+            <div className='main-wrapper'>
+                <ServiceProjects 
+                    serviceName="Web Development" 
+                    serviceColor="#690600" 
+                    projects={projects} 
+                    serviceType="WebDevelopment"
+                />
+            </div>
+            <Footer/>
+            <BackToTop/>
+            <TextMeButton/>
         </>
      );
 }
