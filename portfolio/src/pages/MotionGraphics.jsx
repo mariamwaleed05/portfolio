46552734f4a68dc0cdca45726650b34763a6288c
mariamwaleed05 <mariam.waleed2005@gmail.com>
2025-12-16 18:37:11 +0200

    fix: motiongraphics service page / connected to api

--- portfolio/src/pages/MotionGraphics.jsx
+++ portfolio/src/pages/MotionGraphics.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from "react-helmet";
-
 import SideMenu from '../common/SideMenu';
 import ToggleButtons from './../components/ToggleButtons';
 import BackToTop from '../components/BackToTop';
@@ -8,26 +7,48 @@ import TextMeButton from '../components/TextMeButton';
 import Footer from '../common/Footer';
 import PreLoader from './../common/PreLoader';
 import ServiceProjects from './../components/ServiceProjects';
-import { motionGraphicsProjects } from './../data/services/motionGraphicsProjects';
-
+import { fetchProjectsByCategory } from '../data/services/api';
 
 const MotionGraphics = () => {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadProjects = async () => {
+            try {
+                const data = await fetchProjectsByCategory('MotionGraphics');
+                setProjects(data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadProjects();
+    }, []);
+
+    if (loading) return <PreLoader />;
        
     return ( 
         <>
-        <Helmet><title>Motion Graphics</title>
-             <link rel="icon" type="image/png" href="/icon.png" sizes="16x16" />    
-        </Helmet>
-        <PreLoader/>
-         <SideMenu/>
-        <ToggleButtons/>
-
-<div className='main-wrapper'>
-    <ServiceProjects serviceName="Motion Graphics" serviceColor="#690600" projects={motionGraphicsProjects} />;</div>
-
-        <Footer/>
-        <BackToTop/>
-        <TextMeButton/>
+            <Helmet>
+                <title>Motion Graphics</title>
+                <link rel="icon" type="image/png" href="/icon.png" sizes="16x16" />    
+            </Helmet>
+            <SideMenu/>
+            <ToggleButtons/>
+            <div className='main-wrapper'>
+                <ServiceProjects 
+                    serviceName="Motion Graphics" 
+                    serviceColor="#690600" 
+                    projects={projects} 
+                    serviceType="MotionGraphics"
+                />
+            </div>
+            <Footer/>
+            <BackToTop/>
+            <TextMeButton/>
         </>
      );
 }
