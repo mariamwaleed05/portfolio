76ac364728254f0802096d23773484312b0ac395
mariamwaleed05 <mariam.waleed2005@gmail.com>
2025-12-16 18:38:35 +0200

    fix: content creation service page / connected to api

--- portfolio/src/pages/ContentCreation.jsx
+++ portfolio/src/pages/ContentCreation.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from "react-helmet";
-
 import SideMenu from '../common/SideMenu';
 import ToggleButtons from './../components/ToggleButtons';
 import BackToTop from '../components/BackToTop';
@@ -8,26 +7,48 @@ import TextMeButton from '../components/TextMeButton';
 import Footer from '../common/Footer';
 import PreLoader from './../common/PreLoader';
 import ServiceProjects from './../components/ServiceProjects';
-import { contentCreationProjects } from './../data/services/contentCreationProjects';
+import { fetchProjectsByCategory } from '../data/services/api';
 
 const ContentCreation = () => {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadProjects = async () => {
+            try {
+                const data = await fetchProjectsByCategory('ContentCreation');
+                setProjects(data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadProjects();
+    }, []);
+
+    if (loading) return <PreLoader />;
        
     return ( 
         <>
-        <Helmet><title>Content Creation</title>
-             <link rel="icon" type="image/png" href="/icon.png" sizes="16x16" />    
-        </Helmet>
-        <PreLoader/>
-         <SideMenu/>
-        <ToggleButtons/>
-
-<div className='main-wrapper'>
-               <ServiceProjects serviceName="Content Creation" serviceColor="#690600" projects={contentCreationProjects} />;
-</div>
-        <Footer/>
-        <BackToTop/>
-        <TextMeButton/>
-        
+            <Helmet>
+                <title>Content Creation</title>
+                <link rel="icon" type="image/png" href="/icon.png" sizes="16x16" />    
+            </Helmet>
+            <SideMenu/>
+            <ToggleButtons/>
+            <div className='main-wrapper'>
+                <ServiceProjects 
+                    serviceName="Content Creation" 
+                    serviceColor="#690600" 
+                    projects={projects} 
+                    serviceType="ContentCreation"
+                />
+            </div>
+            <Footer/>
+            <BackToTop/>
+            <TextMeButton/>
         </>
      );
 }
