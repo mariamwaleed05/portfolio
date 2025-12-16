b07213e73d5c28a2a411f5a3c2c8b103150d45f0
mariamwaleed05 <mariam.waleed2005@gmail.com>
2025-12-16 18:36:32 +0200

    fix: photography service page / connected to api

--- portfolio/src/pages/Photography.jsx
+++ portfolio/src/pages/Photography.jsx
@@ -1,37 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from "react-helmet";
-
 import SideMenu from '../common/SideMenu';
 import ToggleButtons from './../components/ToggleButtons';
 import BackToTop from '../components/BackToTop';
 import TextMeButton from '../components/TextMeButton';
 import Footer from '../common/Footer';
-import Title from '../common/Title';
 import PreLoader from './../common/PreLoader';
 import ServiceProjects from './../components/ServiceProjects';
-import { photographyProjects } from '../data/services/photographyProjects';
-
+import { fetchProjectsByCategory } from '../data/services/api';
 
 const Photography = () => {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadProjects = async () => {
+            try {
+                const data = await fetchProjectsByCategory('Photography');
+                setProjects(data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadProjects();
+    }, []);
+
+    if (loading) return <PreLoader />;
 
     return (
         <>
-        <Helmet><title>Photography</title>
-             <link rel="icon" type="image/png" href="/icon.png" sizes="16x16" />    
-        </Helmet>
-        <PreLoader/>
-        <SideMenu/>
-        <ToggleButtons/>
-
-<div className='main-wrapper'>
-      <ServiceProjects serviceName="Photoghraphy" serviceColor="#690600" projects={photographyProjects} />;
-
-</div>
-
-        <Footer/>
-        <BackToTop/>
-        <TextMeButton/>
-        
+            <Helmet>
+                <title>Photography</title>
+                <link rel="icon" type="image/png" href="/icon.png" sizes="16x16" />    
+            </Helmet>
+            <SideMenu/>
+            <ToggleButtons/>
+            <div className='main-wrapper'>
+                <ServiceProjects 
+                    serviceName="Photography" 
+                    serviceColor="#690600" 
+                    projects={projects} 
+                    serviceType="Photography"
+                />
+            </div>
+            <Footer/>
+            <BackToTop/>
+            <TextMeButton/>
         </>
       );
 }
