82e6dc9cda40c3a76e84fed700e6a9277fbdcb34
mariamwaleed05 <mariam.waleed2005@gmail.com>
2025-12-18 00:53:31 +0200

    fix: supabase route in vission & mission

--- portfolio/src/components/MissionVision.jsx
+++ portfolio/src/components/MissionVision.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from './../Supabase';
 
 const MissionVision = () => {
   const [content, setContent] = useState({ mission: '', vision: '' });
